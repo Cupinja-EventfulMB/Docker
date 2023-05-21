1b56be08bea309c6285aab59402ec49098a75831
marijajovanovaa <marijajovanova03@yahoo.com>
2023-05-21 13:00:59 +0200

    EMB-50 updated script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-docker stop container
+env_file="./.env"
+DOCKER_USERNAME=$(grep "DOCKER_USERNAME" "$env_file" | cut -d '=' -f2)
+DOCKER_PASSWORD=$(grep "DOCKER_PASSWORD" "$env_file" | cut -d '=' -f2)
 
-docker login
+docker stop docker_app_1
 
-docker pull cvetankapasinechka/cupinja:project  
+docker rm docker_app_1
 
-docker run -d --name container -p 8080:8080 cvetankapasinechka/cupinja:project
+# Log in to Docker Hub
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "Logged in"
+
+# Docker pull
+docker pull cvetankapasinechka/cupinja:project
+
+# Start the updated container
+docker-compose up -d
+
+# Print the container status
+docker-compose ps
